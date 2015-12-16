618421ca8c5a623747097da3ed1295c8e566efab
Francis <hisconer@gmail.com>
2015-12-16 15:00:39 +0100

    Add jsdoc comment

--- plugins/areGroups.js
+++ plugins/areGroups.js
@@ -1,12 +1,17 @@
-/**
- * Filtra los elementos devolviendo solo los grupos
+/*!
+ * sQuery plugin - areGroups 1.0
+ * Francis Vega
+ *
+ * @desc Filter elements and return just LayerGroups
  * @return {sQuery}
+ * @example
+ * $('%selected%').areGroups().UISelect();
+ * @desc Filter the selection (in Sketchapp UI) and make a selection only with groups.
  */
 
 (function($){
 
   $.fn.areGroups = function() {
-
     var _layers = [];
 
     this.each(function() {
@@ -17,6 +22,6 @@
 
     this.layers = _layers.slice();
     return this;
-
   }
+
 }(sQuery));
