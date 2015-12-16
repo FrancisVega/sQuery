4ee179b7325cab1640c909fbcb37ae257b1165a0
Francis <hisconer@gmail.com>
2015-12-16 14:54:18 +0100

    remove wip plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,3 @@
-'use strict';
-
 //
 // ZEN Sketch Plugin Repo
 // 2015
@@ -8,17 +6,14 @@
 // Gulp itself
 var gulp   = require('gulp');
 var rename = require('gulp-rename');
-var jshint = require('gulp-jshint');
-var plumber = require('gulp-plumber');
+
 // Directorios del proyecto
-//
 var dirs = {
     src: '',
-    dst: '/sketchplugins/ZEN/sQuery',
+    dst: '/sketchplugins/zen.sketchplugin/Contents/Sketch/sQuery',
 };
 
 // Watch
-//
 gulp.task('watch', function(){
     gulp.watch(dirs.src + '**/*', ['copy']);
 });
@@ -26,7 +21,6 @@ gulp.task('watch', function(){
 
 // Copia los plugins en la carpeta de plugins de sketch mediante el softlink
 // /sketchplugins
-//
 gulp.task('copy', function() {
     // Archivos en el raiz
     gulp.src(['plugins', 'sQuery.js'], {cwd: dirs.src})
@@ -36,14 +30,6 @@ gulp.task('copy', function() {
     .pipe(gulp.dest(dirs.dst+"/plugins"));
 });
 
-gulp.task('lint', function() {
-  return gulp.src(dirs.src + '**/*.js')
-    .pipe(plumber())
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
-});
-
-// --------------------------------------------------------------------------{{{
 // TAREAS GULP
 gulp.task('default', ['watch']);
 gulp.task('build', function(callback) {
@@ -51,4 +37,3 @@ gulp.task('build', function(callback) {
         'copy',
         callback);
 });
-// }}}
