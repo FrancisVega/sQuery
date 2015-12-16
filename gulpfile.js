ac461e322e200e177039166b4363dd0cc0d41b06
Francis <hisconer@gmail.com>
2015-12-16 16:40:24 +0100

    small refactor

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,28 @@
-//
-// ZEN Sketch Plugin Repo
-// 2015
-//
+// sQuery gulpfile
 
-// Gulp itself
+// Requires
 var gulp   = require('gulp');
-var rename = require('gulp-rename');
 
-// Directorios del proyecto
+// Paths
 var dirs = {
-    src: '',
-    dst: '/sketchplugins/zen.sketchplugin/Contents/Sketch/sQuery',
+  src: '',
+  // Copy sQuery into zen sketch plugin bundle in order to test sQuery methods
+  dst: '/sketchplugins/zen.sketchplugin/Contents/Sketch/sQuery',
 };
 
 // Watch
 gulp.task('watch', function(){
-    gulp.watch(dirs.src + '**/*', ['copy']);
+  gulp.watch(dirs.src + '**/*', ['copy']);
 });
 
-
-// Copia los plugins en la carpeta de plugins de sketch mediante el softlink
-// /sketchplugins
+// Copy into sketch plugin folder
 gulp.task('copy', function() {
-    // Archivos en el raiz
-    gulp.src(['plugins', 'sQuery.js'], {cwd: dirs.src})
-    .pipe(gulp.dest(dirs.dst));
-
-    gulp.src(['plugins/**/*'], {cwd: dirs.src})
-    .pipe(gulp.dest(dirs.dst+"/plugins"));
+  console.log("Copy sQuery in " + dirs.dst);
+  gulp.src(['plugins', 'sQuery.js'], {cwd: dirs.src})
+  .pipe(gulp.dest(dirs.dst));
+  gulp.src(['plugins/**/*'], {cwd: dirs.src})
+  .pipe(gulp.dest(dirs.dst+"/plugins"));
 });
 
-// TAREAS GULP
-gulp.task('default', ['watch']);
-gulp.task('build', function(callback) {
-    runSequence(
-        'copy',
-        callback);
-});
+// Default task
+gulp.task('default', ['copy','watch']);
