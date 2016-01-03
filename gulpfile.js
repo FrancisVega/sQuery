c14857983dec0aa386f7a48a1931e84309d85a5f
Francis <hisconer@gmail.com>
2016-01-03 22:17:36 +0100

    add test with sTrue.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,16 @@
 // Requires
 var gulp   = require('gulp');
 
+var squeryfile = 'sQuery.js';
+var testfile = 'sQueryTests.cocoascript';
+
 // Paths
 var dirs = {
   src: '',
   // Copy sQuery into zen sketch plugin bundle in order to test sQuery methods
+  test: 'test/',
   dst: '/sketchplugins/zen.sketchplugin/Contents/Sketch/sQuery',
+  testdst: '/sketchplugins/zen.sketchplugin/Contents/Sketch/sQuery/test',
 };
 
 // Watch
@@ -17,13 +22,13 @@ gulp.task('watch', function(){
 
 // Copy into sketch plugin folder
 gulp.task('copy', function() {
-  console.log("Copy sQuery in " + dirs.dst);
-  gulp.src(['plugins', 'sQuery.js', 'sTrue.js', 'sketchCustomPlugin.cocoascript'], {cwd: dirs.src})
+  console.log("Copy "+squeryfile+" in " + dirs.dst);
+  gulp.src(['plugins', 'test', squeryfile], {cwd: dirs.src})
   .pipe(gulp.dest(dirs.dst));
 
-  console.log("Copy sQueryTest in " + dirs.dst);
-  gulp.src(['sQueryTest.cocoascript'], {cwd: dirs.src})
-  .pipe(gulp.dest(dirs.dst));
+  console.log("Copy "+ dirs.test + testfile + " in " + dirs.testdst);
+  gulp.src([testfile], {cwd: dirs.test})
+  .pipe(gulp.dest(dirs.testdst));
 
   gulp.src(['plugins/**/*'], {cwd: dirs.src})
   .pipe(gulp.dest(dirs.dst+"/plugins"));
