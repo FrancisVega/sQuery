9a533801df94dc2083e570d2363f30d4978b47d1
Francis <hisconer@gmail.com>
2016-02-14 19:12:41 +0100

    sync

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,17 @@ var gulp   = require('gulp');
 
 var squeryfile = 'sQuery.js';
 var testfile = 'sQueryTests.js';
+var liveQueryFile = 'liveQuery.js';
 
 // Paths
 var dirs = {
   src: '',
   // Copy sQuery into zen sketch plugin bundle in order to test sQuery methods
   test: 'test/',
+  live: 'liveQuery/',
   dst: '/sketchplugins/zen.sketchplugin/Contents/Sketch/sQuery',
   testdst: '/sketchplugins/zen.sketchplugin/Contents/Sketch/sQuery/test',
+  livedst: '/sketchplugins/zen.sketchplugin/Contents/Sketch/sQuery/liveQuery'
 };
 
 // Watch
@@ -23,8 +26,12 @@ gulp.task('watch', function(){
 // Copy into sketch plugin folder
 gulp.task('copy', function() {
   console.log("Copy "+squeryfile+" in " + dirs.dst);
-  gulp.src(['plugins', 'test', squeryfile], {cwd: dirs.src})
+  gulp.src(['plugins', 'test', 'liveQuery', squeryfile], {cwd: dirs.src})
   .pipe(gulp.dest(dirs.dst));
+
+  console.log("Copy " + dirs.live + liveQueryFile + " in " + dirs.livedst);
+  gulp.src([liveQueryFile], {cwd: dirs.live})
+  .pipe(gulp.dest(dirs.livedst));
 
   console.log("Copy "+ dirs.test + testfile + " in " + dirs.testdst);
   gulp.src([testfile], {cwd: dirs.test})
---
+++ liveQuery/liveQuery.js
@@ -0,0 +1,15 @@
+@import 'sQuery.js';
+/*
+ *@import '../plugins/sQuery.areGroups.js';
+ *@import '../plugins/sQuery.areShapes.js';
+ *@import '../plugins/sQuery.move.js';
+ *@import '../plugins/sQuery.parentRoot.js';
+ *@import '../plugins/sQuery.removeEmptyGroupsRecursive.js';
+ */
+
+
+var doc = context.document;
+log($)
+//var sq = [doc askForUserInput:"sQuery?" initialValue:"*"];
+//$("%"+sq+"%").UISelect();
+
